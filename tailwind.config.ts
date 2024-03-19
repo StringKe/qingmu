6c38b329df26d527526801412f563723ef74e52b
StringKE <authemail@qq.com>
2024-03-19 10:33:38 +0800

    feat: init project

---
+++ tailwind.config.ts
@@ -0,0 +1,15 @@
+import { fontFamily } from 'tailwindcss/defaultTheme';
+
+import { type Config } from 'tailwindcss';
+
+export default {
+    content: ['./src/**/*.tsx'],
+    theme: {
+        extend: {
+            fontFamily: {
+                sans: ['var(--font-sans)', ...fontFamily.sans],
+            },
+        },
+    },
+    plugins: [],
+} satisfies Config;
